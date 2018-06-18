123f944d937e19b68f4ad9123081a32407e1327b
Lucas Hennessy <lucashennessy@gmail.com>
2018-06-18 13:54:38 -0400

    Update formatting Win7.ps1

--- Windows/Win7/Win7.ps1
+++ Windows/Win7/Win7.ps1
@@ -1,5 +1,5 @@
-#Created by Zamanry, 06/2018.
-#Fully functioning as of 06/12/18.
+#Created by Zamanry, 06/02/2018.
+#Fully functioning as of 06/18/2018.
 
 #Import Windows Firewall
 Write-Host "Installing firewall."
@@ -85,7 +85,7 @@ $Services =
 'SCPolicySvc',
 'SNMPTRAP',
 'sppsvc',
-'sppuinotify',
+'sppuinotIfy',
 'SSDPSRV',
 'StorSvc',
 'TabletInputService',
@@ -121,17 +121,17 @@ $Services =
 
 $Index = 0
 $CrntService = $Services[$Index]
-do {
-	if (Get-Service $CrntService -ErrorAction SilentlyContinue) {
+Do {
+	If (Get-Service $CrntService -ErrorAction SilentlyContinue) {
 		Set-Service $CrntService -StartupType "Disabled"
 		Stop-Service $CrntService -Force
 	}
-	else {
+	Else {
 		Write-Host "$CrntService not found."
 	}
 	$Index++
 	$CrntService = $Services[$Index]
-} while ($CrntService -ne $NULL)
+} While ($CrntService -ne $NULL)
 
 #Sets Windows Update service and dependencies to automatic
 Write-Host "Enabling Windows Update."
@@ -141,13 +141,13 @@ Set-Service "TrustedInstaller" -StartupType "Automatic"
 
 #Disable unneccessary network connections:
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services" -Name "DisabledComponents" -Type "DWORD" -Value "0xFF" #Disable IPv6 completely
-$NIC = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'" #Disable 'Register this connection's addresses in DNS'
-$NIC.SetDynamicDNSRegistration($false)
+$NIC = Get-WmiObject Win32_NetworkAdapterConfiguration -Filter "ipenabled = 'true'" #Disable 'Register this connection's addresses in DNS'
+$NIC.SetDynamicDNSRegistration($FALSE)
 $NIC.SetTcpipNetbios(2) #Disable NetBIOS over TCP/IP
 $NIC = [wmiclass]'Win32_NetworkAdapterConfiguration' #Disable LMHosts lookup
-$NIC.enablewins($false,$false)
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "IGMPLevel" -Type "DWORD" -Value "0" #Disable IGMP
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 1 #Disable Remote Desktop
+$NIC.enablewins($FALSE,$FALSE)
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "IGMPLevel" -Type "DWORD" -Value 0 #Disable IGMP
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type "DWORD" –Value 1 #Disable Remote Desktop
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type "DWORD" –Value 0 #Disable Remote Assistance
 
 #Flush caches (DNS, ARP, NetBIOS, routes, hosts):
@@ -160,7 +160,7 @@ Push-Location
 Set-Location "..\..\..\.."
 $DriveLetter = Get-Location
 Pop-Location
-Move-Item -path ".\hosts" -destination "$DriveLetter\Windows\system32\drivers\etc\hosts" -force
+Move-Item -Path ".\hosts" -Destination "$DriveLetter\Windows\system32\drivers\etc\hosts" -Force
 
 #Misc.
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control" -Name "CrashControl" -Type "DWORD" -Value "0x0" #Disable memory dumps
@@ -212,7 +212,7 @@ New-Item -Path "$Path1\Main" -Name "FeatureControl" -Type "Directory" -ErrorActi
 New-Item -Path "$Path1\Main\FeatureControl" -Name "FEATURE_LOCALMACHINE_LOCKDOWN" -Type "Directory" -ErrorAction SilentlyContinue
 Set-ItemProperty -Path "$Path1\Main\FeatureControl\FEATURE_LOCALMACHINE_LOCKDOWN" -Name "iexplore.exe" -Type "DWORD" -Value 1
 Set-ItemProperty -Path "$Path1\Download" -Name "RunInvalid" -Type "DWORD" -Value 0
-Set-ItemProperty -Path "$Path" -Name "CertificateRevocation" -Type "DWORD" -Value 1
+Set-ItemProperty -Path "$Path" -Name "CertIficateRevocation" -Type "DWORD" -Value 1
 Set-ItemProperty -Path "$Path1\Download" -Name "CheckExeSignatures" -Type "String" -Value "yes"
 Set-ItemProperty -Path "$Path\CACHE" -Name "Persistent" -Type "DWORD" -Value 0
 Set-ItemProperty -Path "$Path1\Main" -Name "DOMStorage" -Type "DWORD" -Value 0
@@ -230,58 +230,58 @@ OptionalFeatures
 #Disable outdated protocols:
 Write-Host "Disabling PCT/SSL/TLS outdated protocols."
 $Path = "HKLM:\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
-function DisableProtocol {
-	New-Item -Path "$Path" -Name "$Protocol" -Type Directory -ErrorAction SilentlyContinue
+Function DisableProtocol {
+	New-Item -Path "$Path" -Name "$Protocol" -Type "Directory" -ErrorAction SilentlyContinue
 	$Path = "$Path\$Protocol"
 	New-Item -Path "$Path" -Name "Client" -Type "Directory" -ErrorAction SilentlyContinue
 	New-Item -Path "$Path" -Name "Server" -Type "Directory" -ErrorAction SilentlyContinue
-	Set-ItemProperty -Path "$Path\Client" -Type "DWORD" -Name "DisabledByDefault" -Value 1
-	Set-ItemProperty -Path "$Path\Client" -Type "DWORD" -Name "Enabled" -Value 0
-	Set-ItemProperty -Path "$Path\Server" -Type "DWORD" -Name "DisabledByDefault" -Value 1
-	Set-ItemProperty -Path "$Path\Server" -Type "DWORD" -Name "Enabled" -Value 0
+	Set-ItemProperty -Path "$Path\Client" -Name "DisabledByDefault" -Type "DWORD" -Value 1
+	Set-ItemProperty -Path "$Path\Client" -Name "Enabled" -Type "DWORD" -Value 0
+	Set-ItemProperty -Path "$Path\Server" -Name "DisabledByDefault" -Type "DWORD" -Value 1
+	Set-ItemProperty -Path "$Path\Server" -Name "Enabled" -Type "DWORD" -Value 0
 }
 
 $Protocols = 'PCT 1.0', 'SSL 2.0', 'SSL 3.0', 'TLS 1.0', 'TLS 1.1'
 $Index = 0
 $Protocol = $Protocols[$Index]
-do {
+Do {
 	DisableProtocol
 	$Index++
 	$Protocol = $Protocols[$Index]
-} while ($Protocol -ne $NULL)
+} While ($Protocol -ne $NULL)
 
 #Enable TLS 1.2
 $Protocol = "TLS 1.2"
 New-Item -Path "$Path" -Name "$Protocol" -Type "Directory" -ErrorAction SilentlyContinue
 New-Item -Path "$Path" -Name "Client" -Type "Directory" -ErrorAction SilentlyContinue
 New-Item -Path "$Path" -Name "Server" -Type "Directory" -ErrorAction SilentlyContinue
-Set-ItemProperty -Path "$Path\Client" -Type "DWORD" -Name "DisabledByDefault" -Value 0
-Set-ItemProperty -Path "$Path\Client" -Type "DWORD" -Name "Enabled" -Value 1
-Set-ItemProperty -Path "$Path\Server" -Type "DWORD" -Name "DisabledByDefault" -Value 0
-Set-ItemProperty -Path "$Path\Server" -Type "DWORD" -Name "Enabled" -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" -Type "DWORD" -Name "DefaultSecureProtocols" -Type "DWORD" -Value "0x800"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" -Type "DWORD" -Name "DefaultSecureProtocols" -Type "DWORD" -Value "0x800"
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Type "DWORD" -Name "SecureProtocols" -Type "DWORD" -Value "0x800"
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -Type "DWORD" -Name "chUseStrongCrypto" -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -Type "DWORD" -Name "chUseStrongCrypto" -Value 1
+Set-ItemProperty -Path "$Path\Client" -Name "DisabledByDefault" -Type "DWORD" -Value 0
+Set-ItemProperty -Path "$Path\Client" -Name "Enabled" -Type "DWORD" -Value 1
+Set-ItemProperty -Path "$Path\Server" -Name "DisabledByDefault" -Type "DWORD" -Value 0
+Set-ItemProperty -Path "$Path\Server" -Name "Enabled" -Type "DWORD" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" -Name "DefaultSecureProtocols" -Type "DWORD" -Value "0x800"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" -Name "DefaultSecureProtocols" -Type "DWORD" -Value "0x800"
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name "SecureProtocols" -Type "DWORD" -Value "0x800"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -Name "chUseStrongCrypto" -Type "DWORD" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -Name "chUseStrongCrypto" -Type "DWORD" -Value 1
 $Path = $NULL
 
 #Disable features continued
 Write-Warning "Please uncheck all items except .NET, and Windows Search."
 
-#Disable SMBv1, v2, and v3 (Unnecessary without domain)
+#Disable SMBv1, v2, and v3 (Unnecessary without Domain)
 Write-Host "Disabling all SMB versions."
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Type "DWORD" -Name "SMB1" -Value 0
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Type "DWORD" -Name "SMB2" -Value 0
-sc.exe config lanmanworkstation depend= bowser/mrxsmb20/nsi
-sc.exe config mrxsmb10 start= disabled
-sc.exe config lanmanworkstation depend= bowser/mrxsmb10/nsi
-sc.exe config mrxsmb20 start= disabled
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB1" -Type "DWORD" -Value 0
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "SMB2" -Type "DWORD" -Value 0
+Sc.exe config lanmanworkstation depend= bowser/mrxsmb20/nsi
+Sc.exe config mrxsmb10 start= disabled
+Sc.exe config lanmanworkstation depend= bowser/mrxsmb10/nsi
+Sc.exe config mrxsmb20 start= disabled
 
 #Set UAC level to High
 Write-Host "Setting UAC level to High."
-Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Type "DWORD" -Name "ConsentPromptBehaviorAdmin" -Value 2
-Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Type "DWORD" -Name "PromptOnSecureDesktop" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Type "DWORD" -Value 2
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Type "DWORD" -Value 1
 
 #Enable a custom local security policy
 Write-Host "Setting custom local security policy."
@@ -296,27 +296,27 @@ Stop-Service "msiserver" -Force
 
 #Cleaning up files
 Write-Host "Cleaning up files."
-Remove-Item -path ".\Win7.INF" -ErrorAction SilentlyContinue
-Remove-Item -path ".\Win7.SDB" -ErrorAction SilentlyContinue
-Remove-Item -path ".\Win7.WFW" -ErrorAction SilentlyContinue
-Remove-Item -path "..\Win7.ZIP" -ErrorAction SilentlyContinue
+Remove-Item -Path ".\Win7.INF" -ErrorAction SilentlyContinue
+Remove-Item -Path ".\Win7.SDB" -ErrorAction SilentlyContinue
+Remove-Item -Path ".\Win7.WFW" -ErrorAction SilentlyContinue
+Remove-Item -Path "..\Win7.ZIP" -ErrorAction SilentlyContinue
 
 #Create a standard user
 Write-Warning "Creating a new user. DO NOT name them Admin, Guest, Root, etc."
 $Userverb = Read-Host -Prompt "Enter a new username"
-$ctr = 0
-do {
-	if ($ctr -gt 0) {
+$Ctr = 0
+Do {
+	If ($Ctr -gt 0) {
 		Write-Host " "
 		Write-Warning "Passwords did not match."
 	}
-	$Passverb1 = Read-Host -Prompt "Enter password"-AsSecureString
+	$Passverb1 = Read-Host -Prompt "Enter password" -AsSecureString
 	$Passverb2 = Read-Host -Prompt "Re-enter password" -AsSecureString
-	$ctr++
+	$Ctr++
 	If (([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Passverb1))) -eq ([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Passverb2)))) {
-		$Matched = $True
+		$Matched = $TRUE
 	}
-} while ($Matched -ne $True)
+} While ($Matched -ne $TRUE)
 Net User $Userverb $Passverb1 /Add /Y
 $Userverb, $Passverb1, $Passverb2 = $NULL
 
