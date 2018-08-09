9314ba78ddac8e6a491ac16dd9ed98d912cbd918
Zamanry <lucashennessy@gmail.com>
2018-08-09 16:54:26 -0400

    Added LastWrite.ps1

---
+++ Windows/Win7/LastWrite.ps1
@@ -0,0 +1,23 @@
+#Based off the 08/18/2018 version of https://github.com/asbtho/Powershell-Scripts/blob/master/GetProfilesModifiedWithin.ps1
+
+$Hours = 8
+$Path = (get-location).Drive.Name
+$Path = "${Path}:\"
+$Output = ".\Log.txt"
+Clear-Content $Output -ErrorAction SilentlyContinue
+$ParentDir = Get-ChildItem -Path $Path -Directory -Force -ErrorAction SilentlyContinue
+Write-Warning "Scanning files..."
+
+Foreach ($Dir in $ParentDir) {
+  $ChildList = Get-ChildItem -Path $Path$Dir -Recurse -File -ErrorAction SilentlyContinue
+  Foreach ($Child in $ChildList) {
+    If ($Child.LastWriteTime -GT (Get-Date).AddHours(-$Hours)) {
+      $CurrentTime = Get-Date
+      $PathOutput = $Child.FullName
+      $TimeOutput = $CurrentTime - $Child.LastWriteTime
+      $OutputString = "$PathOutput`t`t| Last write: $TimeOutput"
+      Out-File -Append -Filepath $Output -InputObject $OutputString
+      Break
+    }
+  }
+}
